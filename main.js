61db6b07e4ae0b3d98affcdafc03bbc9069acd51
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2022-06-10 15:33:34 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const opener = require("opener");
-require('dotenv').config();
+const opener = require("opener"); /* Declares the library 'opener'. Allows us to use it in this file. */
+require('dotenv').config(); /* Declares and configures library 'dotenv', which allows us to use environmental variables stored in a '.env' file. */
 
-console.log("Hello, " + process.env.name + "!");
+console.log("Hello, " + process.env.name + "!"); /* Sends a message to the console(terminal) using our '.env' variable 'name'. */
 
-opener(process.env.url);
+opener(process.env.url); /* Opens a website in a new tab using our 'url' variable stored in '.env'. */
